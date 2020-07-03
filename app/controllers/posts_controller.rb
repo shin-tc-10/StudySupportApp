55f097e916d061ba7f9340f97186226b7b724308
shin-nkt-09 <shin.nkt.09@gmail.com>
2020-07-03 16:29:01 +0900

    投稿メニューボタン追記

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,10 +13,32 @@ def create
   redirect_to posts_path
 end
 
+def edit
+  @post = Post.find(params[:id])
+
+end
+
+
+def update
+  post = Post.find(params[:id])
+  post.update(post_params)
+  redirect_to root_path
+end
+
+
+def destroy
+  post = Post.find(params[:id])
+  post.destroy
+  redirect_to posts_path
+end
+
+def show
+end
+
 private
 
 def post_params
-  params.require(:post).permit(:user_id, :text, :title).merge(user_id: current_user.id)
+  params.require(:post).permit(:text, :title).merge(user_id: current_user.id)
 end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   root 'top#index'
-  resources :posts, only: [:index, :new, :create]
+  resources :posts
   resources :timers,only: [:index]
 end
